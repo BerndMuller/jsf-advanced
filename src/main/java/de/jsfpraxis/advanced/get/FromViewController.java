563d4a8723b4eb43e78045d83e7700e78fb0a2a2
Bernd Mueller <bernd.mueller@ostfalia.de>
2020-09-30 16:42:25 +0200

    Flash completed

--- src/main/java/de/jsfpraxis/advanced/get/FromViewController.java
+++ src/main/java/de/jsfpraxis/advanced/get/FromViewController.java
@@ -1,6 +1,10 @@
 package de.jsfpraxis.advanced.get;
 
 import javax.enterprise.context.RequestScoped;
+import javax.faces.application.FacesMessage;
+import javax.faces.context.ExternalContext;
+import javax.faces.context.FacesContext;
+import javax.inject.Inject;
 import javax.inject.Named;
 
 @Named
@@ -8,14 +12,32 @@ import javax.inject.Named;
 public class FromViewController {
 
 	private String input;
+	
+	@Inject
+	FacesContext facesContext;
+	
+	@Inject
+	ExternalContext externalContext;
 
 	public FromViewController() {
 	}
 
 	public String save() {
+		saveToDatabase(input); // dummy operation
+		externalContext.getFlash().put("input", input);
 		return "to-view.xhtml?faces-redirect=true";
 	}
 	
+	public String propagateMessage() {
+		facesContext.addMessage(null, new FacesMessage("Eine wichtige FacesMessage"));
+		externalContext.getFlash().setKeepMessages(true);
+		return "to-view.xhtml?faces-redirect=true";
+	}
+	
+	private void saveToDatabase(String input) {
+		// hier ein Fake. Denken Sie z.B. an JPA, siehe Kapitel Classic Models, o.ä.
+	}
+
 	// Getter und Setter
 	public String getInput() {
 		return input;
--- src/main/java/de/jsfpraxis/advanced/get/ToViewController.java
+++ src/main/java/de/jsfpraxis/advanced/get/ToViewController.java
@@ -1,15 +1,66 @@
 package de.jsfpraxis.advanced.get;
 
+import java.util.Map;
+import java.util.logging.Logger;
+
+import javax.annotation.PostConstruct;
 import javax.enterprise.context.RequestScoped;
+import javax.faces.annotation.ManagedProperty;
+import javax.faces.annotation.RequestCookieMap;
+import javax.faces.context.ExternalContext;
+import javax.faces.context.Flash;
+import javax.inject.Inject;
 import javax.inject.Named;
+import javax.servlet.http.Cookie;
 
 @Named
 @RequestScoped
 public class ToViewController {
-
+	
+	private static final Logger logger = Logger.getLogger(ToViewController.class.getCanonicalName());
+	
+	@Inject
+	//@ManagedProperty("#{flash.keep.input}")
+	@ManagedProperty("#{flash.input}")
+	private String inputFromOtherController;
+	
+	@Inject
+	ExternalContext externalContext;
+	
+	@Inject
+	Flash flash; // mit Map-Interface
+	
+	@Inject
+	@RequestCookieMap
+	Map<String, Object> cookieMap;
+	
 	public ToViewController() {
 	}
 
+	public String getInputFromOtherController1() {
+		return inputFromOtherController;
+	}
 	
+	public String getInputFromOtherController2() {
+		//externalContext.getFlash().keep("input");
+		return (String) externalContext.getFlash().get("input");
+	}
 	
+	public String getInputFromOtherController3() {
+		//flash.keep("input");
+		return (String) flash.get("input");
+	}
+	
+	@PostConstruct
+	public void init() {
+		System.out.println("redirect: " + flash.isRedirect());
+		System.out.println("keep messages: " + flash.isKeepMessages());
+		flash.entrySet().forEach(e -> System.out.println("flash: " + e.getKey() + " " + e.getValue()));
+		cookieMap.entrySet().forEach(e -> System.out.println("cookieMap: " + e.getKey() + " " + e.getValue()));
+		printCookie((Cookie) cookieMap.get("csfcfc"));
+	}
+	
+	private void printCookie(Cookie cookie) {
+		logger.info(cookie.getName());
+	}
 }
